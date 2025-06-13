36f6f7abb2287a1ac1164bec2934c51e08c9660a
Santiago Paz <santipaz19.02@gmail.com>
2025-06-13 19:07:30 -0300

    fix to deploy

--- src/modules/employee/controllers/index.ts
+++ src/modules/employee/controllers/index.ts
@@ -1,7 +1,7 @@
 
 export { createEmployee } from './employeeCreate';
 export { deleteEmployee } from './employeeDelete';
-export { getAllEmployees } from './employeegetAll';
+export { getAllEmployees } from './employeeGetAlls';
 export { updateEmployee } from './employeeUpdate';
 export { getEmployee } from './employeeGetById';
 
